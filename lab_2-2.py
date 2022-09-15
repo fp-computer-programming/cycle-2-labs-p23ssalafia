5086b2cd3667cab6613c56e0f037602b1b47eb95
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-09-15 10:57:08 -0400

    Submission for lab 2-2

---
+++ lab_2-2.py
@@ -0,0 +1,12 @@
+#Author : S.S 9/15/22
+a = 3
+b = a
+#both variables should be 3. If we know "a" is 3 and "b" is equal to "a" then "b" must be 3.
+print (a)
+print (b)
+#confirmed a and b are 3
+
+a = 7
+print (a)
+print (b)
+#Because a now becomes 7 later in the code, the value of a changes to 7, but because b isn't equal to a now, it remains 3.
