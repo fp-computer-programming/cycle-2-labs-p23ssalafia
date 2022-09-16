bbd193233b5e844ce2c3c1a63a24685deae1c764
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-09-16 12:13:38 -0400

    Submission for lab 2-4

---
+++ lab_2-4.py
@@ -0,0 +1,16 @@
+# Author S.S 9/16/2022
+
+a = 1
+b = 2
+c = 3
+x = 2
+y = 5
+
+v1 = (3 + 4*x)/5
+print (v1)
+v2 = 10 * ((y-5) * (a + b + c)**2)/5
+print (v2)
+v3 = 9 * ((4/x**2) + ((9 + x)/y))
+print (v3)
+final_answer = v1 - v2 + v3 
+print (final_answer)
