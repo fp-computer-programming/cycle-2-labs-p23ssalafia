2c55d800ed540964d50b7cbf2db7d000e813c1f7
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-09-20 13:46:06 -0400

    Submission for lab 2-7

---
+++ lab_2-7.py
@@ -0,0 +1,59 @@
+#Author: Sean Salafia 9/20/22
+
+a = 5
+b = 10
+c = 0
+d = -4
+
+if a % 2 == 0:
+    print ('a is an even number')
+else:
+    print ('a is an odd number')
+
+if a < -5:
+    print('a is is less than -5')
+elif a > 5:
+    print('a is greater than 5')
+else:
+    print('a is between -5 and 5')
+#---------------------------------------------------------------------------
+
+if b % 2 == 0:
+    print ('b is an even number')
+else:
+    print ('b is an odd number')
+
+if b < -5:
+    print('b is is less than -5')
+elif b > 5:
+    print('b is greater than 5')
+else:
+    print('b is between -5 and 5')
+#---------------------------------------------------------------------------
+
+if c % 2 == 0:
+    print ('c is an even number')
+else:
+    print ('c is an odd number')
+
+if c < -5:
+    print('c is is less than -5')
+elif c > 5:
+    print('c is greater than 5')
+else:
+    print('c is between -5 and 5')
+#---------------------------------------------------------------------------
+
+if d % 2 == 0:
+    print ('d is an even number')
+else:
+    print ('d is an odd number')
+
+if d < -5:
+    print('d is is less than -5')
+elif d > 5:
+    print('d is greater than 5')
+else:
+    print('d is between -5 and 5')
+
+print (a,b,c,d)
