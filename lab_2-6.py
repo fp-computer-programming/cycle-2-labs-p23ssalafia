be7249d39b8fb58a9fe06cb4c0fdae9fb90ae48b
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-09-20 13:45:53 -0400

    Submission for Lab 2-6

---
+++ lab_2-6.py
@@ -0,0 +1,38 @@
+#Author: Sean Salafia 9/20/22
+
+
+a = 5
+b = 10
+c = 0
+d = -4
+
+if a % 2 == 0:
+    print ('a is an even number')
+    print (a)
+else:
+    print ('a is an odd number')
+    print (a)
+
+
+if b % 2 == 0:
+    print ('b is an even number')
+    print (b)
+else:
+    print ('b is an odd number')
+    print (b)
+
+
+if c % 2 == 0:
+    print ('c is an even number')
+    print (c)
+else:
+    print ('c is an odd number')
+    print (c)
+
+
+if d % 2 == 0:
+    print ('d is an even number')
+    print (d)
+else:
+    print ('d is an odd number')
+    print (d)
