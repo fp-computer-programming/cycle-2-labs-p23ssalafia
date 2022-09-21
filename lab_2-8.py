cf11501f8c072ec7a6d4877db4ef6f19de124e24
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-09-21 14:16:12 -0400

    Submission for Lab 2-8

---
+++ lab_2-8.py
@@ -0,0 +1,16 @@
+#Author: Sean Salafia 9/21/22
+
+x = 8
+
+
+
+if x <= 8: 
+    print('Give up on your dreams')
+else:
+    if x <= 11:
+        print('Almost! You got a bronze medal!')
+    else:
+        if x <= 14:
+            print('Next time! You got a silver medal!')
+        else: 
+            print('Congrats! You won gold!')
